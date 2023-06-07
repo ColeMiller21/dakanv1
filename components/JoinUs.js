a53755e9dc50b51e0244648f8b35aba66d5b3b8f
Cole Miller <cole.t.miller@gmail.com>
2023-06-07 09:20:34 -0500

    updated join us section

--- components/JoinUs.js
+++ components/JoinUs.js
@@ -5,9 +5,9 @@ import DotsBanner from "./DotsBanner";
 const JoinUs = () => {
   return (
     <>
-      <div className="w-screen min-h-auto flex flex-col justify-center items-center mb-[100px] overflow-hidden lg:max-w-7xl">
+      <div className="w-full min-h-auto flex flex-col justify-center items-center mb-[100px] overflow-hidden lg:max-w-7xl">
         <DotsBanner />
-        <div className="w-full md:w-[60%] my-[7rem]">
+        <div className="w-full md:w-[60%] my-[7rem] ">
           <p className="text-[2rem] lg:text-[3rem] text-center">
             Where physical and digital{" "}
             <span className="text-primary-green">collect</span> together.
