d9eb5177cde6b8aca7f763c913d82fe4c9aa62d2
cole miller <cole.t.miller@gmail.com>
2023-08-10 10:14:57 -0500

    updated with twitter event script

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
 import Head from "next/head";
+import Script from "next/script";
 
 export const metadata = {
   other: {
@@ -25,6 +26,14 @@ export default function RootLayout({ children }) {
         {children}
         <Footer />
       </body>
+      <Script>
+        {`
+        !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+        },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+        a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+        twq('config','ofsn5');
+        `}
+      </Script>
     </html>
   );
 }
